cdccd07c684edb2f6180933941819100b41f7854
DamienLN <99394425+DamienLN@users.noreply.github.com>
2022-07-29 17:57:23 +0200

    p6.start-change

--- back/app.js
+++ back/app.js
@@ -25,5 +25,6 @@ app.use((req, res, next) => {
 app.use('/api/stuff', stuffRoutes);
 app.use('/api/auth', userRoutes);
 app.use('/images', express.static(path.join(__dirname, 'images')));
+
 module.exports = app;
 
