dff047c1ca2732c746bb9eeade4f717c763827be
DamienLN <99394425+DamienLN@users.noreply.github.com>
2022-07-29 17:46:41 +0200

    p6.2

--- back/app.js
+++ back/app.js
@@ -1,24 +1,29 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+const Thing = require('./models/thing');
 const app = express();
+const userRoutes = require('./routes/user');
+const stuffRoutes = require('./routes/stuff');
+
+app.use('/api/stuff', stuffRoutes);
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://admin:admin@cluster0-pme76.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 
 app.use((req, res, next) => {
-  console.log('Requête reçue !');
-  next();
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
 });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
-
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
+app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
+
