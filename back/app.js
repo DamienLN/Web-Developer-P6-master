d09a87ef77222347361af206457a868ae629cfdc
DamienLN <99394425+DamienLN@users.noreply.github.com>
2022-08-01 11:05:20 +0200

    P6 avant like

--- back/app.js
+++ back/app.js
@@ -1,30 +1,38 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Thing = require('./models/thing');
-const app = express();
-const userRoutes = require('./routes/user');
-const stuffRoutes = require('./routes/stuff');
 const path = require('path');
-app.use('/api/stuff', stuffRoutes);
-app.use(express.json());
+const cors = require('cors')
+// Routes //
+const userRoutes = require('./routes/user');
+const sauceRoutes = require('./routes/sauce');
 
+// Connexion a mongoDB //
 mongoose.connect('mongodb+srv://admin:admin@cluster0.tkexwrw.mongodb.net/piquante?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+// Lancement d express //
+const app = express();
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-});
-
-app.use('/api/stuff', stuffRoutes);
-app.use('/api/auth', userRoutes);
+// Header CORS //
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+//     next();
+// });
+app.use(cors())
+// Permet d afficher les images //
 app.use('/images', express.static(path.join(__dirname, 'images')));
+
+// Transition en json //
+app.use(express.json());
+
+// Lancement des routes //
+app.use('/api/sauces', sauceRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
 
