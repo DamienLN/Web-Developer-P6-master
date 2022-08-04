03e572062d073b33150260c98c6582bdd08edc46
DamienLN <99394425+DamienLN@users.noreply.github.com>
2022-08-04 09:42:28 +0200

    up 1.2

--- back/app.js
+++ back/app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const path = require('path');
 const cors = require('cors')
 
+const rateLimit = require("express-rate-limit")
 // Routes //
 const userRoutes = require('./routes/user');
 const sauceRoutes = require('./routes/sauce');
@@ -14,11 +15,20 @@ mongoose.connect('mongodb+srv://admin:admin@cluster0.tkexwrw.mongodb.net/piquant
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000,
+  max: 1000, // a modifier selon la demande
+  message: "Trop de tentatives de connexion. Compte bloqué pour 5 minutes"
+})
+
+
 // Lancement d express //
 const app = express();
 
 // Header CORS //
 app.use(cors())
+
+app.use(limiter)
 
 // Permet d afficher les images //
 app.use('/images', express.static(path.join(__dirname, 'images')));
@@ -29,6 +39,11 @@ app.use(express.json());
 // Lancement des routes //
 app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
+
+
+
+
+
 
 module.exports = app;
 
